58da9cd3065e22f63995be05f691bf950ae2bcf9
BurnedBySnow <sebastian.karlsson97@gmail.com>
2022-11-28 17:32:13 +0100

    changes to prohibit duplicate id

--- src/utils/addLocationCard.ts
+++ src/utils/addLocationCard.ts
@@ -1,11 +1,14 @@
-import { useDispatch } from "react-redux";
+import { nanoid } from "nanoid";
+import { useDispatch, useSelector } from "react-redux";
 import { addLocation } from "../reducers/locationSlice";
 import { setSearchData } from "../reducers/searchSlice";
-import { store } from "../store";
+import { RootState, store } from "../store";
 import { Location } from "../types";
 import { addWeatherData } from "./addWeatherData";
 
 export const addLocationCard = async (item: Location) => {
+  const temp = store.getState().locations;
+
   const location: Location = {
     id: item.id,
     name: item.name,
@@ -14,6 +17,11 @@ export const addLocationCard = async (item: Location) => {
     latitude: item.latitude,
     longitude: item.longitude,
   };
+  temp.forEach((obj) => {
+    if (location.id === obj.id) {
+      location.id = nanoid();
+    }
+  });
   store.dispatch(addLocation(location));
 
   addWeatherData(location.latitude, location.longitude, location.id);
